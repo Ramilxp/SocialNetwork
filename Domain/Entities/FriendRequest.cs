e6ce113f8952dcea8bba03524bf5f1a04c19196b
LAPTOP-TO31IR5G\ramil <ramilxp2009@mail.ru>
2016-10-26 23:23:05 +0300

    Создал новый пустой проект

--- Domain/Entities/FriendRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Domain.Entities
-{
-    public class FriendRequest
-    {
-        public int Id { get; set; }
-        public int UserId { get; set; }
-        public int PossibleFriendId { get; set; }
-    }
-}
